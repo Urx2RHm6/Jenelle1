8be3ba195bfc8bc94f0a2715d112fd5fa971238a
Urx2RHm6 <russell@unseelieventures.com>
2020-02-11 19:30:59 -0600

    Initial commit

---
+++ src/example.js
@@ -0,0 +1,133 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+
+const GridExampleCelled = () => (
+  // If you touch anything above this line you will break it!
+  <Grid celled>
+    <Grid.Row>
+      <Grid.Column width={5} />
+      <Grid.Column width={8} />
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column width={10} />
+      <Grid.Column width={3} />
+      
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={5} />
+      <Grid.Column width={5} />
+      <Grid.Column width={5} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={5} />
+      <Grid.Column width={5} />
+      <Grid.Column width={5} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={3} />
+      
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+      <Grid.Column width={6} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+    </Grid.Row>
+    <Grid.Row>
+      <Grid.Column width={8} />
+      <Grid.Column width={4} />
+      <Grid.Column width={4} />
+    </Grid.Row>
+    <Grid.Row>
+    <Grid.Column width={12}/>
+    <Grid.Column width={1}/>
+      </Grid.Row>
+      <Grid.Row>
+      <Grid.Column width={12} />
+      <Grid.Column width={1} />
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={10} />
+        </Grid.Row>
+        <Grid.Row>
+        <Grid.Column width={10} />
+        </Grid.Row>
+        <Grid.Row>
+        <Grid.Column width={10} />
+        </Grid.Row>
+
+
+   
+  </Grid>
+ 
+
+  // If you touch anything below this line you will break it!
+);
+
+export default GridExampleCelled;
